a7b1c4138ccc514f8feafff9aba0df2d6ca20943
vitormoschetti <vitormoschetti@terra.com.br>
2025-04-30 08:09:11 -0300

    [POC] encurtador de url

---
+++ url-shortener-sample/src/main/java/br/com/urlshortenersample/UrlShortenerSampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.urlshortenersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenerSampleApplication.class, args);
+    }
+
+}
---
+++ url-shortener-sample/src/test/java/br/com/urlshortenersample/UrlShortenerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.urlshortenersample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
